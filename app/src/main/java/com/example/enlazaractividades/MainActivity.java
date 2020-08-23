b89bb0323040ea6fdf61bc335f7e9649aec08618
musicoariel4 <musicoariel4@gmail.com>
2020-08-23 14:08:56 -0500

    pruevba de git

---
+++ app/src/main/java/com/example/enlazaractividades/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.enlazaractividades;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+//import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private  EditText  mMessageEditText ;
+    public static final String EXTRA_MESSAGE = "com.example.android.Acerca.extra.MESSAGE";
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    public static final int TEXT_REQUEST = 1; // código de solicitud
+
+    private TextView mReplyHeadTextView;
+    private TextView mReplyTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mMessageEditText=(EditText)findViewById(R.id.txt_nombre);
+        mReplyHeadTextView = findViewById(R.id.text_header_reply);
+        mReplyTextView = findViewById(R.id.text_message_reply);
+
+
+    }
+
+
+    public void onActivityResult(int requestCode,int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == TEXT_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                String reply = data.getStringExtra(Acerca.EXTRA_REPLY);
+                mReplyHeadTextView.setVisibility(View.VISIBLE);
+                mReplyTextView.setText(reply);
+                mReplyTextView.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+
+    public void launchSecondActivity(View view) {
+
+        Log.d(LOG_TAG, "Button clicked!");
+        Intent intent = new Intent(this, Acerca.class);
+        String message = mMessageEditText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivityForResult(intent, TEXT_REQUEST); //envio de intent con codigo de solicitud
+    }
+}
